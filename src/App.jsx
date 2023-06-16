25144c214c0649170d279da79167e90d616f2e08
Vittorio <90337009+vittoopugliese@users.noreply.github.com>
2023-06-16 20:47:54 -0300

    euro blue

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,38 @@
-import { useState } from "react";
+import {useEffect, useState} from "react";
 
 function App() {
-  const [time, setTime] = useState('y')
-  const [plata , setPlata] = useState(0)
-  const [res , setRes] = useState(0)
+  const [time, setTime] = useState("y");
+  const [plata, setPlata] = useState(0);
+  const [res, setRes] = useState(0);
+  const [blue, setBlue] = useState(0);
+  const [euro, setEuro] = useState(0);
 
-  function getPlata(){
-    let platita
+  async function getBlue(){
+    const data = await fetch("https://api.bluelytics.com.ar/v2/latest")
+    .then(res => res.json())
+    setBlue(data.blue.value_avg)
+    setEuro(data.blue_euro.value_avg)
+  }
 
-    platita = plata / time
+  getBlue()
 
-    if(time == 'a') return plata * 12
+  function getPlata() {
+    let platita;
 
-    return platita ? platita : 0
+    platita = plata / time;
+
+    if (time == "a") return plata * 12;
+
+    return platita ? platita : 0;
   }
 
   return (
     <div className="container">
-      <input type="number" placeholder="Tu sueldo MENSUAL" onChange={(e) => setPlata(e.target.value)} />
+      <input
+        type="number"
+        placeholder="Tu sueldo MENSUAL"
+        onChange={(e) => setPlata(e.target.value)}
+      />
       <select value={time} onChange={(e) => setTime(e.target.value)}>
         <option value="-">queres saber cuanto ganas por:</option>
         <option value="a">Ano</option>
@@ -29,7 +44,8 @@ function App() {
         <option value="2628000000">(extremo) MILI-Segundo</option>
       </select>
       <h1>= {getPlata()}</h1>
-      <h1>{getPlata() * 480} crocantes</h1>
+      <h1>{getPlata() * blue} peso</h1>
+      <h1>{getPlata() * euro} euro</h1>
     </div>
   );
 }
